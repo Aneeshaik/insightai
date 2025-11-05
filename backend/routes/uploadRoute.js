5eaba0745ca597aec17c2e198df1a82392ff30bf
Aneeshaik <aneesnafeesimmu@gmail.com>
2025-11-05 12:01:33 +0530

    changes

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import multer from 'multer'
 import path from 'path'
+import fs from 'fs'
 import { extractTextFromPdf } from '../services/documentService.js';
 import { createVectorStoreFromText } from '../services/llmService.js';
 
