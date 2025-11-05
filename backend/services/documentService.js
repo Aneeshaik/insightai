cde79eadbae469df53d1310af37e58ecfd92ec57
Aneeshaik <aneesnafeesimmu@gmail.com>
2025-11-05 12:23:19 +0530

    changes

--- backend/services/documentService.js
+++ backend/services/documentService.js
@@ -1,12 +1,11 @@
-import fs from 'fs'
-import { PDFParse } from 'pdf-parse';
+import fs from 'fs';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const pdf = require('pdf-parse');
 
 export async function extractTextFromPdf(filePath) {
-    const dataBuffer = fs.readFileSync(filePath)
-    const dataArray = new Uint8Array(dataBuffer)
-    const parser = new PDFParse(dataArray, {
-        standardFontDataUrl: 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.16.105/standard_fonts/' // Use a CDN for pdf.js fonts
-    })
-    const pdfData = await parser.getText();
-    return pdfData.text
+  const dataBuffer = fs.readFileSync(filePath);
+  const pdfData = await pdf(dataBuffer);
+  return pdfData.text;
 }
