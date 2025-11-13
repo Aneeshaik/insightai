56e85c64adf29fb25684286579c5d154e5a3ce74
Aneeshaik <aneesnafeesimmu@gmail.com>
2025-11-13 11:08:00 +0530

    changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-none fixed top-0 text-white p-4 h-12 flex justify-between items-center w-full">
+    <nav className="bg-none fixed top-env(safe-area-inset-top) text-white p-4 h-12 flex justify-between items-center w-full">
       <p className="text-lg font-semibold">Paper Brain</p>
       <span className="text-sm text-gray-300">Chat with your documents</span>
     </nav>
